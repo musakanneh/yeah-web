8ff77577e09a137cc795550d69007d6556554757
musakanneh <mkanneh17@alustudent.com>
2020-03-23 13:39:17 +0200

    updated minor fixes

--- yeah-web/resources/js/scripts.js
+++ yeah-web/resources/js/scripts.js
@@ -44,18 +44,53 @@ $(document).ready(function() {
 });
 
 // for API
-function firstCall(e) {
-  e.preventDefault();
+// function firstCall(e) {
+//   e.preventDefault();
 
-  var htmlForm = document.getElementById("formSample");
-  var form = new FormData(htmlForm);
-  form.append("url", "http://api.theyeah.io/api/v1/waiting_list_post");
+//   var htmlForm = document.getElementById("formSample");
+//   var form = new FormData(htmlForm);
+//   form.append("url", "http://api.theyeah.io/api/v1/waiting_list_post");
 
-  form.forEach((e, i) => {
-    console.log(i, e);
-  });
-}
+//   form.forEach((e, i) => {
+//     console.log(i, e);
+//   });
+// }
 
-$(document).ready(function() {
-  $("#buttonClick").on("click", "button", firstCall);
+// $(document).ready(function() {
+//   $("#buttonClick").on("click", "button", firstCall);
+// });
+var form = new FormData();
+form.append("name", "Patrick Sikalinda");
+form.append("email", "patrick3@theyeah.io");
+form.append("phone_number", "250780698185");
+
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "http://api.theyeah.io/api/v1/waiting_list_post",
+  method: "POST",
+  headers: {
+    authorization: "Basic MjUwNzgwNjk4MTg4OjEyMzQ=",
+    "cache-control": "no-cache",
+    "postman-token": "fd5734a9-216d-9b25-a63a-f59640707bc9"
+  },
+  processData: false,
+  contentType: false,
+  mimeType: "multipart/form-data",
+  data: form
+};
+
+$.ajax(settings).done(function(response) {
+  console.log(response);
+});
+
+// --------------------------
+// typer js
+// ----------------------------
+var typed = new Typed(".type", {
+  // alert("here");
+  strings: ["easier‌", "quicker", "simpler"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true
 });
