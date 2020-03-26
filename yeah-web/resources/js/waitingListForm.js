bee6fb1976004400eec25844e32b4dd85c10d84c
musakanneh <mkanneh17@alustudent.com>
2020-03-26 22:28:16 +0200

    contents updated

--- yeah-web/resources/js/waitingListForm.js
+++ yeah-web/resources/js/waitingListForm.js
@@ -1,96 +1,90 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $("#buttonClick").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone_number = $("#phone_number").val();
 
+    // $.post("http://api.theyeah.io/api/v1/waiting_list_post",
+    //     {
+    //         name: name,
+    //         phone_number: phone_number,
+    //         email: email,
+    //     },
+    //     function(data,status){
+    //         alert("I'm clikced");
+    //         alert("Data: " + data + "\nStatus: " + status);
+    //     });
 
-    $("#buttonClick").click(function(){
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var phone_number = $("#phone_number").val();
+    $.ajax({
+      type: "GET",
+      url: "https://www.theyeah.io/waitinglist",
+      data: {
+        name: name,
+        phone_number: phone_number,
+        email: email
+      },
 
-        // $.post("http://api.theyeah.io/api/v1/waiting_list_post",
-        //     {
-        //         name: name,
-        //         phone_number: phone_number,
-        //         email: email,
-        //     },
-        //     function(data,status){
-        //         alert("I'm clikced");
-        //         alert("Data: " + data + "\nStatus: " + status);
-        //     });
+      contentType: "application/x-www-form-urlencoded",
+      success: function(responseData, textStatus, jqXHR) {
+        if (responseData == 1) {
+          Swal.fire({
+            icon: "success",
+            title: "Completed",
+            text:
+              "Thanks for expressing interest in Yeah, we are excited to create a product for you.",
+            footer:
+              '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
+          });
 
-        $.ajax({
-            type: "GET",
-            url: "https://www.theyeah.io/waitinglist",
-            data: {
-                name: name,
-                phone_number: phone_number,
-                email: email,
-            },
-            contentType: "application/x-www-form-urlencoded",
-            success: function(responseData, textStatus, jqXHR) {
+        } else if (responseData == 2) {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text:
+              "Sorry, an error occurred. Kindly refresh your browser and resubmit the form.",
+            footer:
+              '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
+          });
 
-                if(responseData == 1){
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Completed',
-                        text: 'Thanks for expressing interest in Yeah, we are excited to create a product for you.',
-                        footer: '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
-                    });
-                }
-                else if(responseData == 2){
+        } else if (responseData == 5) {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "A user with that email already exists.",
+            footer:
+              '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
+          });
 
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: "Sorry, an error occurred. Kindly refresh your browser and resubmit the form.",
-                        footer: '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
-                    });
-                }
+        } else if (responseData == 6) {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "A user with that phone number already exists.",
+            footer:
+              '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
+          });
 
-                else if(responseData == 5){
-
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: "A user with that email already exists.",
-                        footer: '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
-                    });
-                }
-
-                else if(responseData == 6){
-
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: "A user with that phone number already exists.",
-                        footer: '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
-                    });
-                }
-
-                else{
-
-                    Swal.fire({
-                        icon: 'Error',
-                        title: 'Incomplete',
-                        text: "Complete the missing fields",
-                        footer: '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Sorry, an error occurred. Kindly refresh your browser and resubmit the form.',
-                    footer: '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
-                });
-            }
-        })
-
-
-
+        } else {
+          Swal.fire({
+            icon: "Error",
+            title: "Incomplete",
+            text: "Complete the missing fields",
+            footer:
+              '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
+          });
+        }
+      },
+      
+      error: function(jqXHR, textStatus, errorThrown) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text:
+            "Sorry, an error occurred. Kindly refresh your browser and resubmit the form.",
+          footer:
+            '<a href="mailto:yeah@theyeah.io?Subject=Question" target="_top">Have questions?</a>'
+        });
+      }
     });
-
-
+  });
 });
-
-
--- yeah-web/waitinglist/index.php
+++ yeah-web/waitinglist/index.php
@@ -2,11 +2,10 @@
 
 include_once "connection.php";
 
-if(!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email'])){
-
+if (!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email'])) {
 
     //email exists
-    if (record_exists ( 'waitinglist', 'email', $_GET['email'],$conn)){
+    if (record_exists('waitinglist', 'email', $_GET['email'], $conn)) {
         //email is free now check phone number
 
         //check phone number exists
@@ -14,26 +13,20 @@ if(!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email
         //email is free now check phone number
 
         //check phone number exists
-        if (record_exists ( 'waitinglist', 'phone_number', $_GET['phone_number'],$conn)){
+        if (record_exists('waitinglist', 'phone_number', $_GET['phone_number'], $conn)) {
             //it does not insert
             insertUser($conn);
-        }
-
-        else{
-
+        } else {
             //if exists echo 6
             echo 6;
         }
-
     } else {
         //if exists echo 5
         echo 5;
     }
 
     //done if row exists
-}
-
-else{
+} else {
 
     //echo three for unseen error
 
@@ -46,11 +39,10 @@ function insertUser($conn){
     $email = mysqli_real_escape_string($conn, $_GET['email']);
     $phone_number = mysqli_real_escape_string($conn, $_GET['phone_number']);
 
-    if(mysqli_query($conn, "INSERT INTO waitinglist(name, email, phone_number) VALUES('" . $name . "', '" . $email . "', '" . $phone_number . "')")) {
+    if (mysqli_query($conn, "INSERT INTO waitinglist(name, email, phone_number) VALUES('" . $name . "', '" . $email . "', '" . $phone_number . "')")) {
         //echo one for success
         echo 1;
     } else {
-
         //echo 2 error on our side
         echo 2;
     }
@@ -59,25 +51,17 @@ function insertUser($conn){
 }
 
 
-
-
 //Check if a value exists in a table
-function record_exists ($table, $column, $value,$conn) {
+function record_exists($table, $column, $value, $conn)
+{
     $query = "SELECT * FROM waitinglist WHERE $column = '$value'";
 
-    $result=mysqli_query($conn,$query);
-    $rowcount=mysqli_num_rows($result);
+    $result = mysqli_query($conn, $query);
+    $rowcount = mysqli_num_rows($result);
 
-
-    if($rowcount == 0){
-
+    if ($rowcount == 0) {
         return TRUE;
-    }
-
-    else{
+    } else {
         return FALSE;
     }
-
 }
-
-
