7d82742a70822d7841e76ff0af0ff5b8aeb7b501
musakanneh <mkanneh17@alustudent.com>
2020-03-26 21:50:05 +0200

    contents updated

--- yeah-web/waitinglist/connection.php
+++ yeah-web/waitinglist/connection.php
@@ -7,4 +7,3 @@ $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
     die('Could not Connect MySql Server:' .mysql_error());
 }
-?>
--- yeah-web/waitinglist/index.php
+++ yeah-web/waitinglist/index.php
@@ -2,17 +2,11 @@
 
 include_once "connection.php";
 
-
 if(!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email'])){
 
 
-
-
     //email exists
-
     if (record_exists ( 'waitinglist', 'email', $_GET['email'],$conn)){
-
-
         //email is free now check phone number
 
         //check phone number exists
@@ -21,41 +15,22 @@ if(!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email
 
         //check phone number exists
         if (record_exists ( 'waitinglist', 'phone_number', $_GET['phone_number'],$conn)){
-
-
             //it does not insert
             insertUser($conn);
-
         }
 
         else{
 
             //if exists echo 6
             echo 6;
-
-
         }
 
-
-
-
     } else {
-
-
-
         //if exists echo 5
         echo 5;
-
-
-
-
-
     }
 
     //done if row exists
-
-
-
 }
 
 else{
@@ -90,7 +65,6 @@ function insertUser($conn){
 function record_exists ($table, $column, $value,$conn) {
     $query = "SELECT * FROM waitinglist WHERE $column = '$value'";
 
-
     $result=mysqli_query($conn,$query);
     $rowcount=mysqli_num_rows($result);
 
@@ -98,11 +72,9 @@ function record_exists ($table, $column, $value,$conn) {
     if($rowcount == 0){
 
         return TRUE;
-
     }
 
     else{
-
         return FALSE;
     }
 
