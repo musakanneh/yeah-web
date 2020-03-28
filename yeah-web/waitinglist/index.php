d13861a762fcb4ff7602e50eb8c865b568b5809a
patricksikalinda <sikalindapatrick@gmail.com>
2020-03-28 21:14:02 +0200

    form changes Patrick

--- yeah-web/waitinglist/index.php
+++ yeah-web/waitinglist/index.php
@@ -3,19 +3,18 @@ header("Access-Control-Allow-Origin: *");
 
 include_once "connection.php";
 
-<<<<<<< HEAD
-if (!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email'])) {
-=======
 
 
 if(!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email'])){
 
 
 
->>>>>>> 654f9d1a2cbcb515e8a8b8a842d0ec706c5dde78
 
     //email exists
-    if (record_exists('waitinglist', 'email', $_GET['email'], $conn)) {
+
+    if (record_exists ( 'waitinglist', 'email', $_GET['email'],$conn)){
+
+
         //email is free now check phone number
 
         //check phone number exists
@@ -23,20 +22,45 @@ if(!empty($_GET['name']) && !empty($_GET['phone_number']) && !empty($_GET['email
         //email is free now check phone number
 
         //check phone number exists
-        if (record_exists('waitinglist', 'phone_number', $_GET['phone_number'], $conn)) {
+        if (record_exists ( 'waitinglist', 'phone_number', $_GET['phone_number'],$conn)){
+
+
             //it does not insert
             insertUser($conn);
-        } else {
+
+        }
+
+        else{
+
             //if exists echo 6
             echo 6;
+
+
         }
+
+
+
+
     } else {
+
+
+
         //if exists echo 5
         echo 5;
+
+
+
+
+
     }
 
     //done if row exists
-} else {
+
+
+
+}
+
+else{
 
     //echo three for unseen error
 
@@ -49,10 +73,11 @@ function insertUser($conn){
     $email = mysqli_real_escape_string($conn, $_GET['email']);
     $phone_number = mysqli_real_escape_string($conn, $_GET['phone_number']);
 
-    if (mysqli_query($conn, "INSERT INTO waitinglist(name, email, phone_number) VALUES('" . $name . "', '" . $email . "', '" . $phone_number . "')")) {
+    if(mysqli_query($conn, "INSERT INTO waitinglist(name, email, phone_number) VALUES('" . $name . "', '" . $email . "', '" . $phone_number . "')")) {
         //echo one for success
         echo 1;
     } else {
+
         //echo 2 error on our side
         echo 2;
     }
@@ -61,17 +86,27 @@ function insertUser($conn){
 }
 
 
+
+
 //Check if a value exists in a table
-function record_exists($table, $column, $value, $conn)
-{
+function record_exists ($table, $column, $value,$conn) {
     $query = "SELECT * FROM waitinglist WHERE $column = '$value'";
 
-    $result = mysqli_query($conn, $query);
-    $rowcount = mysqli_num_rows($result);
 
-    if ($rowcount == 0) {
+    $result=mysqli_query($conn,$query);
+    $rowcount=mysqli_num_rows($result);
+
+
+    if($rowcount == 0){
+
         return TRUE;
-    } else {
+
+    }
+
+    else{
+
         return FALSE;
     }
+
 }
+
